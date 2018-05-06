b91c7027e2f71509521a820981a3255959fe079f
Alfred Arouna <alfred+git@arouna.net>
2018-05-06 13:07:18 +0100

    Update README

--- config.inc.php
+++
@@ -1,7 +0,0 @@
-// Password Plugin options
-// -----------------------
-$config['password_freeipa_host'] = 'ipa0.iso-ad.isocel.bj'; 
-$config['password_freeipa_cert'] = $ROUNDCUBE_ROOT.'/plugins/password/drivers/freeipa/certs/ipa0.iso-ad.isocel.bj_ca.crt'; 
-$config['password_freeipa_admin'] = 'admin';
-$config['password_freeipa_admin_passwd'] = 'password';
-
